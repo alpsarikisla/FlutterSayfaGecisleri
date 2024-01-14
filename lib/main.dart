11529bfacf3900dc6cecec36aa4f91e2b68b544f
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-01-14 12:19:42 +0300

    Veri İletimi ve Tasarım değiştirildi

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,12 @@ class GirisEkrani extends StatefulWidget {
 }
 
 class _GirisEkraniState extends State<GirisEkrani> {
+  TextEditingController ctrlKullaniciAdi = TextEditingController();
+  TextEditingController ctrlSifre = TextEditingController();
   girisYap() {
-    Navigator.pushNamed(context, "/ProfilSayfasi");
+    Navigator.pushNamed(context, "/ProfilSayfasi",
+        arguments: DataModel(
+            kullaniciAdi: ctrlKullaniciAdi.text, sifre: ctrlSifre.text));
   }
 
   @override
@@ -42,9 +46,61 @@ class _GirisEkraniState extends State<GirisEkrani> {
     return Scaffold(
       appBar: AppBar(title: const Text("Giriş Ekranı")),
       body: Container(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
         child: Column(
           children: <Widget>[
-            ElevatedButton(onPressed: girisYap, child: const Text("Giriş Yap")),
+            const Image(
+                image: AssetImage("lib/images/loginimage.jpg"), width: 150),
+            //const Divider(),
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.purple, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                hintText: 'Kullanıcı Adı Yazınız',
+              ),
+              controller: ctrlKullaniciAdi,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.purple, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                hintText: 'Şifre Yazınız',
+              ),
+              controller: ctrlSifre,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: girisYap,
+              child: const Text("Giriş Yap"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: const TextStyle(
+                  //fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -66,15 +122,26 @@ class _ProfilEkraniState extends State<ProfilEkrani> {
 
   @override
   Widget build(BuildContext context) {
+    DataModel gelenVeri =
+        ModalRoute.of(context)?.settings.arguments as DataModel;
     return Scaffold(
       appBar: AppBar(title: const Text("Profil Sayfası")),
       body: Container(
         child: Column(
           children: <Widget>[
-            ElevatedButton(onPressed: cikisYap, child: const Text("Çıkış Yap")),
+            ElevatedButton(
+                onPressed: cikisYap,
+                child: const Text("Çıkış Yap")), //elevatedbutton
+            Text(gelenVeri.kullaniciAdi),
+            Text(gelenVeri.sifre),
           ],
         ),
       ),
     );
   }
+}
+
+class DataModel {
+  String kullaniciAdi, sifre;
+  DataModel({required this.kullaniciAdi, required this.sifre});
 }
